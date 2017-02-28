666e1e0ac00a11b3cf53c2e879c99ebff3afe191
massat <masato.hirai@gmail.com>
2017-02-28 23:51:17 +0900

    MySQLから値を返す

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
+require 'json'
+require 'mysql2'
 require 'sinatra'
 
 class App < Sinatra::Base
 
   get '/' do
-    'Hello world!'
+    mysql = Mysql2::Client.new(
+      host: 'mysql',
+      username: 'app',
+      password: 'app',
+      database: 'app'
+    )
+    entries = mysql.query('SELECT * FROM `book`').entries
+
+    JSON.generate(entries)
   end
 
 end
